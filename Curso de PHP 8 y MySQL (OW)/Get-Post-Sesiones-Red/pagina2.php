b85490fa667851f46b58cb76425586d775104213
AlexandruKovacs <98741717+AlexandruKovacs@users.noreply.github.com>
2023-01-24 21:43:20 +0100

    Delete Curso de PHP 8 y MySQL (OW) directory

--- Curso de PHP 8 y MySQL (OW)/Get-Post-Sesiones-Red/pagina2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "pagina 2";
-echo '<br>';
-
-print_r($_GET);
-
-header("Location: pagina3.php?name=" . $_GET['name']);
--- Curso de PHP 8 y MySQL (OW)/Get-Post-Sesiones-Red/sesiones1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION['mi_sesion'] = "VALOR";
-
-?>
-
-<a href="sesiones2.php">Pruebas de sesiones</a>
