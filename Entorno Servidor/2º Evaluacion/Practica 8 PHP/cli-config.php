4189118e422102aeb00c21f8a3b37f7fd54d6070
AlexandruKovacs <axkovacs27@gmail.com>
2023-01-25 08:45:57 +0100

    modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica 8 PHP/cli-config.php"

--- Entorno Servidor/2º Evaluacion/Practica 8 PHP/cli-config.php
+++ Entorno Servidor/2º Evaluacion/Practica 8 PHP/cli-config.php
@@ -2,7 +2,7 @@
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-// replace with file to your own project bootstrap.php
+// replace with file to your own project bootstrap
 require_once 'bootstrap.php';
 return ConsoleRunner::createHelperSet($entityManager);
 
