539c34343c639044660d2cd09cc843a4f6641971
AlexandruKovacs <axkovacs27@gmail.com>
2023-01-25 09:35:10 +0100

    modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica 7 PHP/buscar-id.php"

--- Entorno Servidor/2º Evaluacion/Practica 7 PHP/buscar-id.php
+++ Entorno Servidor/2º Evaluacion/Practica 7 PHP/buscar-id.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('./src/Equipo.php');
+require_once('./entities/Equipo.php');
 require_once('bootstrap.php');
 
-$eq = $entityManager->find(':Equipo', 1);
+$eq = $entityManager->find('Equipo', 1);
 
 echo $eq->getSocios();
 
@@ -11,7 +11,7 @@ $eq->setSocios(70000);
 
 $entityManager->flush();
 
-$eq = $entityManager->find(':Equipo', 4);
+$eq = $entityManager->find('Equipo', 4);
 
 if (!$eq) {
     echo 'Equipo no encontrado';
