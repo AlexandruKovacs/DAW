76f0e554378a7eb085abbffdde84dac466faf326
AlexandruKovacs <axkovacs27@gmail.com>
2023-02-11 18:55:11 +0100

    modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica Obligatoria 11/index.php" 	modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica Obligatoria 11/js/main.js"

--- Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/index.php
+++ Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/index.php
@@ -7,6 +7,9 @@ require_once './entities/Proveedor.php';
 $selectProveedor = $entityManager->createQuery('SELECT pro FROM Proveedor pro');
 $proveedores = $selectProveedor->getResult();
 
+$selectArticulos = $entityManager->createQuery('SELECT art FROM Articulos art');
+$articulos = $selectArticulos->getResult();
+
 ?>
 
 <!DOCTYPE html>
@@ -23,9 +26,10 @@ $proveedores = $selectProveedor->getResult();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h2 class="mt-3">Artículos</h2>
-    <div class="container d-block w-25 mb-5">
-        <form id="form-filtros">
+    <h2 class="mt-3">AlmaGest</h2>
+    <div class="container w-25 mb-4">
+        <form id="formFiltros">
+
             <label for="filtroPrecio">Filtrar por precio:</label>
             <select id="filtroPrecio" class="form-control mb-3">
                 <option value="">Todos</option>
@@ -43,12 +47,94 @@ $proveedores = $selectProveedor->getResult();
             </select>
 
             <label for="filtroCantidad">Filtrar por cantidad:</label>
-            <select id="filtroCantidad" class="form-control mb-3">
-                <option value="">Todos</option>
-                <option value="baja">Baja</option>
-                <option value="media">Media</option>
-                <option value="alta">Alta</option>
+            <div class="d-flex gap-2">
+                <select id="filtroCantidad" class="form-control">
+                    <option value="">Todos</option>
+                    <option value="baja">Baja</option>
+                    <option value="media">Media</option>
+                    <option value="alta">Alta</option>
+                </select>
+                <button class="btn btn-primary" id="botonOpciones"><i class="fa-sharp fa-solid fa-arrow-down"></i></button>
+            </div>
+
+        </form>
+    </div>
+
+    <div id="masOpciones">
+        <button class="btn btn-warning" id="modificarCantidad">Modificar cantidad <i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="btn btn-success" id="anadirArticulo">Añadir artículo <i class="fa-solid fa-plus"></i></button>
+        <button class="btn btn-danger" id="borrarArticulo">Borrar artículo <i class="fa-solid fa-trash"></i></button>
+    </div>
+
+    <div class="container w-25 justify-content-center mt-5" id="cajaModificar">
+        <h5 class="text-center mb-4">Modificar artículo</h5>
+        <form id="formModificar">
+
+            <label for="articuloModificar">Selecciona el articulo:</label>
+            <select id="articuloModificar" class="form-control mb-3" required>
+                <option value="">Selecciona un artículo</option>
+                <?php foreach ($articulos as $articulo) : ?>
+                    <option value="<?php echo $articulo->getId(); ?>">
+                    <?php echo $articulo->getNombre() . ' - ' .  $articulo->getCantidad() . ' unidades'; ?>
+                    </option>
+                <?php endforeach; ?>
             </select>
+            <div id="cajaCantidad">
+                <label for="cantidadModificar">Nueva cantidad:</label>
+                <div class="d-flex gap-2">
+                    <input type="number" class="form-control" value="2" required>
+                    <button class="btn btn-success w-25" id="actualizarCantidad"><i class="fa-solid fa-check"></i></button>
+                    <button class="btn btn-danger" id="cerrarModificar"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="container w-25 justify-content-center mt-5" id="cajaAnadir">
+        <h5 class="text-center mb-4">Añadir articulo</h5>
+        <form id="formAnadir">
+
+            <label for="articuloAnadir">Selecciona el articulo:</label>
+            <select id="articuloAnadir" class="form-control mb-3" required>
+                <option value="">Selecciona un artículo</option>
+                <?php foreach ($articulos as $articulo) : ?>
+                    <option value="<?php echo $articulo->getId(); ?>">
+                    <?php echo $articulo->getNombre() . ' - ' .  $articulo->getCantidad() . ' unidades'; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <div id="cantidadAnadir">
+                <label for="cantidadAnadir">Nueva cantidad:</label>
+                <div class="d-flex gap-2">
+                    <input type="number" class="form-control" value="2" required>
+                    <button class="btn btn-success w-25" id="actualizarAnadir"><i class="fa-solid fa-check"></i></button>
+                    <button class="btn btn-danger" id="cerrarAnadir"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="container w-25 justify-content-center mt-5" id="cajaBorrar">
+        <h5 class="text-center mb-4">Borrar articulo</h5>
+        <form id="formModificar">
+
+            <label for="articuloModificar">Selecciona el articulo:</label>
+            <select id="articuloModificar" class="form-control mb-3" required>
+                <option value="">Selecciona un artículo</option>
+                <?php foreach ($articulos as $articulo) : ?>
+                    <option value="<?php echo $articulo->getId(); ?>">
+                    <?php echo $articulo->getNombre() . ' - ' .  $articulo->getCantidad() . ' unidades'; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <div id="cajaCantidad">
+                <label for="cantidadModificar">Nueva cantidad:</label>
+                <div class="d-flex gap-2">
+                    <input type="number" class="form-control" value="2" required>
+                    <button class="btn btn-success w-25" id="actualizarBorrar"><i class="fa-solid fa-check"></i></button>
+                    <button class="btn btn-danger" id="cerrarBorrar"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+            </div>
         </form>
     </div>
     <div id="cajaTabla">
--- Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/js/main.js
+++ Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/js/main.js
@@ -3,12 +3,29 @@ const filtroProveedor = document.getElementById('filtroProveedor');
 const filtroCantidad = document.getElementById('filtroCantidad');
 const tablaArticulos = document.getElementById('tablaArticulos');
 const cajaTabla = document.getElementById('cajaTabla');
+const botonOpciones = document.getElementById('botonOpciones');
+const masOpciones = document.getElementById('masOpciones');
+const cajaModificar = document.getElementById('cajaModificar');
+const cajaAnadir = document.getElementById('cajaAnadir');
+const cajaBorrar = document.getElementById('cajaBorrar');
+const modificarCantidad = document.getElementById('modificarCantidad');
+const anadirArticulo = document.getElementById('anadirArticulo');
+const borrarArticulo = document.getElementById('borrarArticulo');
+const actualizar = document.getElementById('actualizar');
+const cerrarModificar = document.getElementById('cerrarModificar');
+const cerrarAnadir = document.getElementById('cerrarAnadir');
+const cerrarBorrar = document.getElementById('cerrarBorrar');
 
 let xhr = new XMLHttpRequest();
 
 document.addEventListener('DOMContentLoaded', function(){
-    cajaTabla.style.display = 'none';
+    masOpciones.style.display = 'none';
+    cajaModificar.style.display = 'none';
+    cajaAnadir.style.display = 'none';
+    cajaBorrar.style.display = 'none';
+    cajaTabla.style.visibility = 'hidden';
 });
+
 
 filtroPrecio.addEventListener('change', function(event) {
     let precio = event.target.value;
@@ -22,12 +39,13 @@ filtroPrecio.addEventListener('change', function(event) {
             tbody.innerHTML = '';
             tbody.innerHTML = articulosFiltrados;
 
-            cajaTabla.style.display = 'block';
+            cajaTabla.style.visibility = 'visible';
 
         }
     };
     xhr.send();
 });
+
 
 filtroProveedor.addEventListener('change', function(event) {
     let idProveedor = event.target.value;
@@ -41,12 +59,13 @@ filtroProveedor.addEventListener('change', function(event) {
             tbody.innerHTML = '';
             tbody.innerHTML = articulosFiltrados;
 
-            cajaTabla.style.display = 'block';
+            cajaTabla.style.visibility = 'visible';
 
         }
     };
     xhr.send();
 });
+
 
 filtroCantidad.addEventListener('change', function(event) {
     let cantidad = event.target.value;
@@ -60,9 +79,55 @@ filtroCantidad.addEventListener('change', function(event) {
             tbody.innerHTML = '';
             tbody.innerHTML = articulosFiltrados;
 
-            cajaTabla.style.display = 'block';
+            cajaTabla.style.visibility = 'visible';
 
         }
     };
     xhr.send();
+});
+
+
+botonOpciones.addEventListener('click', function (e) {
+    e.preventDefault();
+    let icon = botonOpciones.querySelector('i');
+
+    if (icon.classList.contains('fa-arrow-down')) {
+        icon.classList.remove('fa-arrow-down');
+        icon.classList.add('fa-arrow-up');
+        masOpciones.style.display = 'block';
+    } else {
+        icon.classList.remove('fa-arrow-up');
+        icon.classList.add('fa-arrow-down');
+        masOpciones.style.display = 'none';
+    }
+});
+
+modificarCantidad.addEventListener('click', function () {
+    cajaModificar.style.display = 'block';
+    cajaAnadir.style.display = 'none';
+    cajaBorrar.style.display = 'none';
+});
+
+anadirArticulo.addEventListener('click', function () {
+    cajaModificar.style.display = 'none';
+    cajaAnadir.style.display = 'block';
+    cajaBorrar.style.display = 'none';
+});
+
+borrarArticulo.addEventListener('click', function () {
+    cajaModificar.style.display = 'none';
+    cajaAnadir.style.display = 'none';
+    cajaBorrar.style.display = 'block';
+});
+
+cerrarModificar.addEventListener('click', function () {
+    cajaModificar.style.display = 'none';
+});
+
+cerrarAnadir.addEventListener('click', function () {
+    cajaAnadir.style.display = 'none';
+});
+
+cerrarBorrar.addEventListener('click', function () {
+    cajaBorrar.style.display = 'none';
 });
