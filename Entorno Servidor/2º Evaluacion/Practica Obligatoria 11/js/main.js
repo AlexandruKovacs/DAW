0b98e69dc46ba1d9c5f4834e6120ae4c64dc469f
AlexandruKovacs <axkovacs27@gmail.com>
2023-02-13 22:36:04 +0100

    new file:   "Entorno Servidor/2\302\272 Evaluacion/Practica Obligatoria 11/js/consts.js" 	modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica Obligatoria 11/js/main.js" 	modified:   "Entorno Servidor/2\302\272 Evaluacion/Practica Obligatoria 11/index.html"

--- Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/js/main.js
+++ Entorno Servidor/2º Evaluacion/Practica Obligatoria 11/js/main.js
@@ -1,29 +1,3 @@
-const botonOpciones = document.getElementById('botonOpciones');
-const masOpciones = document.getElementById('masOpciones');
-const cajaModificar = document.getElementById('cajaModificar');
-const cajaAnadir = document.getElementById('cajaAnadir');
-const cajaBorrar = document.getElementById('cajaBorrar');
-const cajaBorrarProveedor = document.getElementById('cajaBorrarProveedor');
-const modificarCantidad = document.getElementById('modificarCantidad');
-const anadirArticulo = document.getElementById('anadirArticulo');
-const borrarArticulo = document.getElementById('borrarArticulo');
-const proveedorBorrar = document.getElementById('proveedorBorrar');
-const articuloBorrar = document.getElementById('articuloBorrar');
-const articuloModificar = document.getElementById('articuloModificar');
-const borrar = document.getElementById('borrar');
-const modificar = document.getElementById('modificar');
-const anadir = document.getElementById('anadir');
-const botonBorrarProveedor = document.getElementById('botonBorrarProveedor');
-const borrarProveedor = document.getElementById('borrarProveedor');
-const cerrarModificar = document.getElementById('cerrarModificar');
-const cerrarAnadir = document.getElementById('cerrarAnadir');
-const cerrarBorrar = document.getElementById('cerrarBorrar');
-const cerrarBorrarProveedor = document.getElementById('cerrarBorrarProveedor');
-const forms = document.getElementsByTagName('form');
-const respuesta = document.getElementById('respuesta');
-
-console.log(forms);
-
 let xhr = new XMLHttpRequest();
 
 function obtenerProveedores() {
