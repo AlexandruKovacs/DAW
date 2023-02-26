f333c260f32eb329bd7294e8e5914c38ded9ca18
AlexandruKovacs <axkovacs27@gmail.com>
2023-02-26 18:03:27 +0100

    modified:   GestorIncidencias/css/style.css 	modified:   GestorIncidencias/menu-admin.php

--- GestorIncidencias/menu-admin.php
+++ GestorIncidencias/menu-admin.php
@@ -38,11 +38,29 @@ if (!isset($_SESSION['usuario'])) {
             </a>
         </div>
         <div class="info-container">
-            Bienvenido/a <?php echo $_SESSION['nombreApellidos']; ?>.
+            Bienvenido/a <?php echo $_SESSION['usuario']; ?>.
             <a class="logout" href="server/cerrar-sesion.php"><i class="fa-solid fa-right-from-bracket fa-xl"></i></a>
         </div>
     </nav>
 
-    <h2>Administrador</h2>
+    <div class="menu-principal">
+        <div>
+            <a href="">
+                <button id="crearIncidencia" class="crear-incidencia">
+                    <i class="fa-solid fa-bars-progress"></i>
+                </button>
+            </a>
+            <p>Incidencias en proceso</p>
+        </div>
+
+        <div>
+            <a href="">
+                <button id="listarIncidencias" class="listar-incidencias">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+            </a>
+            <p>Modificar estado</p>
+        </div>
+    </div>
 </body>
 </html>
